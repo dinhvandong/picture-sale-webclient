1abe76e0ca3a72a52e2defa7b0b0cacf73d34ea9
DUC31032004 <ductranquang3102k4@gmail.com>
2024-12-20 15:56:59 +0700

    update about
    EM Duc update About

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         
+        
         {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
