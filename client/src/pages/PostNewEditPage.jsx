80becf65daba60db7e45717145fa6db274195025
dinhvandong <dongvan.ict3@gmail.com>
2025-01-16 15:53:51 +0700

    update code

---
+++ client/src/pages/PostNewEditPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostNewEditPage = () => {
+  return (
+    <div>PostNewEditPage</div>
+  )
+}
+
+export default PostNewEditPage
---
+++ client/src/pages/PostNewsCreatePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostNewsCreatePage = () => {
+  return (
+    <div>PageNewsCreatePage</div>
+  )
+}
+
+export default PostNewsCreatePage
